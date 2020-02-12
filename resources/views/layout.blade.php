f3a5782234dcf1d90c7d278dd3cde6bff995a626
Spikoov <hem.jyh@gmail.com>
2020-02-12 18:12:30 +0100

    added welcome page

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    <meta charset="utf-8">
+    <title>Projet TP RIA</title>
+  </head>
+  <body>
+      @yield('content')
+  </body>
+</html>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,7 @@
+@extends('layout')
+
+@section('content')
+<div class="w3-display-middle">
+    <h1>Bienvenue</h1>
+</div>
+@endsection
