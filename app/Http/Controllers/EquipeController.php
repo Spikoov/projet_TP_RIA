74ccf5145a46333defc00d36153b0839e0811445
Spikoov <hem.jyh@gmail.com>
2020-05-24 16:14:09 +0200

    opsie

--- app/Http/Controllers/EquipeController.php
+++ app/Http/Controllers/EquipeController.php
@@ -6,13 +6,6 @@ use Illuminate\Http\Request;
 
 class EquipeController extends Controller
 {
-
-<<<<<<< HEAD
-  public function generateEquipe($value='')
-  {
-
-  }
-=======
     public function generateEquipe()
     {
         $this->_titulaires = getTitulaires();
@@ -29,5 +22,4 @@ class EquipeController extends Controller
     {
         //insert...
     }
->>>>>>> acbcf19eaa2ecef867a25ac96fd40660517e03f0
 }
