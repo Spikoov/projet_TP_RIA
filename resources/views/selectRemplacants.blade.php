9e5ba3ee491ceb209f7107d6cf30218a142c527b
Spikoov <hem.jyh@gmail.com>
2020-05-30 16:04:20 +0200

    update style

--- resources/views/selectRemplacants.blade.php
+++ resources/views/selectRemplacants.blade.php
@@ -5,7 +5,7 @@
     {{ csrf_field() }}
     <table id="table" class="w3-table w3-hoverable w3-bordered">
         <thead class="w3-light-blue">
-            <th id="resetSort" class="w3-button w3-hover-cyan" type="button">&#8634;<div id="budgetRempl">0ß</div></th>
+            <th id="resetSort" class="w3-button w3-hover-cyan" type="button">&#8634;<br><div class="w3-badge w3-green" id="budgetRempl">0ß</div></th>
             <th>Nom du joueur</th>
             <th id="sort0" class="w3-button w3-hover-cyan" type="button">Age &#8597;</th>
             <th id="sort1" class="w3-button w3-hover-cyan" type="button">Poste &#8597;</th>
@@ -52,7 +52,13 @@ function countChecked(){
       if(budgetRemplacants>{{ $budgetEquipe }})
       {
         $('#boutonValider').attr('disabled', 'disabled');
-        $('#budgetRempl').css('background-color', 'red');
+
+        $('#budgetRempl').removeClass("w3-green");
+        $('#budgetRempl').addClass("w3-red");
+      }
+      else{
+          $('#budgetRempl').removeClass("w3-red");
+          $('#budgetRempl').addClass("w3-green");
       }
 }
 
